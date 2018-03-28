6ebd044ba1a7a16f64e084c07b7e1ace5331283c
Stefano Montesi <dincio.montesi@gmail.com>
2018-03-28 04:05:05 -0400

    Finished custom enable_if implementation and worked up random example.

--- cpp_conf/cppcon/2014/mtmp_compendium/EnableIf/main.cpp
+++ cpp_conf/cppcon/2014/mtmp_compendium/EnableIf/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <type_traits>
 
+/***********************************/
+/* custom enable if implementation */
+/***********************************/
+template <bool, typename T = void>
+struct enable_if {
+    using type = T;
+};
+
+template <typename T>
+struct enable_if<false, T> {};
+
+template <bool C, typename T>
+using enable_if_t = typename enable_if<C, T>::type;
+
+
+/***********************************************/
+/* shitty precision-accounting equals function */
+/***********************************************/
+// integral types
+template <typename Rhs, typename Lhs>
+enable_if_t<
+    std::is_integral_v<
+        std::common_type_t<Rhs, Lhs>>, bool>
+equals(Rhs rhs, Lhs lhs) {
+    return rhs == lhs;
+}
+
+// floating point types
+template <typename Rhs, typename Lhs>
+enable_if_t<
+    std::is_floating_point_v<
+        std::common_type_t<Rhs, Lhs>>, bool>
+equals(Rhs rhs, Lhs lhs) {
+    static const double delta = 0.01;
+    return std::abs(rhs - lhs) < delta;
+}
+
+// MAIN
 int main() {
-    std::cout << "hello world" << std::endl;
+    std::cout << equals(10.00001 - 9, 1) << std::endl;
 
     return 0;
 }
