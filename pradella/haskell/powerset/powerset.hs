644e7e87422ce6ec4c30b760928eb691cf56b204
Stefano Montesi <dincio.montesi@gmail.com>
2019-10-15 17:30:04 +0200

    Also added tail recursion version of powerset

--- pradella/haskell/powerset/powerset.hs
+++ pradella/haskell/powerset/powerset.hs
@@ -1,6 +1,16 @@
+-- Naive version
 powerset :: Show a => [a] -> [[a]]
 powerset [] = [[]]
 powerset (x:xs) = powerset xs ++ [x:ss | ss <- powerset xs]
 
+-- Tail recursive version
+tailPowerset lst = powerset lst [[]] where
+    powerset [] out = out
+    powerset (x:xs) out = powerset xs (out ++ [x:ss | ss <- out])
+
 main :: IO ()
-main = print $ powerset [1, 2, 3]
+main = do
+    print "naive: "
+    print $ powerset [1, 2, 3]
+    print "with tail recursion: "
+    print $ tailPowerset [1, 2, 3]
